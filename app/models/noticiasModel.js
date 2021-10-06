22cd879d8b194a85ea3e7dca21c43aef435dba26
Diego Belisario <diego.dbs@pf.gov.br>
2021-10-06 11:00:38 -0300

    Teste

--- app/models/noticiasModel.js
+++ app/models/noticiasModel.js
@@ -1,13 +1,24 @@
-module.exports = function(app){
+function Noticias(connection){
 
-	this.getNoticias = function(connection, callback){
-		connection.query('SELECT * FROM noticias', callback);
-	};
+	this._connection = connection;
 
-	this.getNoticia = function(connection, callback){
-		connection.query('SELECT * FROM noticias where id_noticias = 1', callback);
-	};
+}
 
-	return this;
+Noticias.prototype.getNoticias = function(callback){
+	this._connection.query('SELECT * FROM noticias', callback);
+};
+
+Noticias.prototype.getNoticia = function(callback){
+	this._connection.query('SELECT * FROM noticias where id_noticias = 1', callback);
+};
+
+Noticias.prototype.salvarNoticia = function(noticia, callback){ 
+	this._connection.query('INSERT INTO noticias SET ?', noticia, callback); 
+
+};
+
+module.exports = function(){
+
+	return Noticias;
 
 }
